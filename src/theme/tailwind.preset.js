0d7eafc1ea8b0c6edd79066f6f18db60b2b577e9
Artjoms Atroscenko <artjoms.atroscenko@magebit.com>
2022-03-04 13:00:25 +0200

    rewrite css to talwind

--- src/theme/tailwind.preset.js
+++ src/theme/tailwind.preset.js
@@ -5,6 +5,9 @@ module.exports = {
                 dark: '#1F39FF',
                 light: '#5266FF',
                 pressed: '#1C33E6'
+            },
+            white:{
+                active: '#F4F5F5'
             }
         }
     }
